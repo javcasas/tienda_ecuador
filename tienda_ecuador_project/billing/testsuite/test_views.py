56b417fcdbe055f00e7d96e42e8384f77cd0c8f2
Javier Casas <javcasas@gmail.com>
2015-05-09 22:34:09 -0500

    Starting proper TDD

--- tienda_ecuador_project/billing/testsuite/test_views.py
+++ tienda_ecuador_project/billing/testsuite/test_views.py
@@ -1,1 +1,73 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from billing import models
+
+
+#class BillingIndexViewTests(TestCase):
+#
+#    def test_index_view(self):
+#        """
+#        """
+#        response = self.client.get(reverse('index'))
+#        self.assertEqual(response.status_code, 200)
+#        self.assertContains(response, "There are no categories present.")
+#        self.assertQuerysetEqual(response.context['categories'], [])
+
+#class ItemViewTests(TestCase):
+#    def test_non_existing_item_view(self):
+#        """
+#        Ensure a non-existing item returns a 404
+#        """
+#        response = self.client.get(reverse('view_item', args=(555,)))
+#        self.assertEqual(response.status_code, 404)
+
+class NotLoggedInTests(TestCase):
+    """
+    Ensure no useful information can be get without logging in
+    """
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_company_index(self):
+        response = self.client.get(reverse('company_index', kwargs={'company_id': 1}))
+        self.assertEqual(response.status_code, 302)
+
+
+class LoggedInTests(TestCase):
+    """
+    Tests that support a logged-in user
+    """
+    def setUp(self):
+        self.user = User(username="paco")
+        self.user.set_password("paco_pw")
+        self.user.save()
+        self.c = Client()
+        r = self.c.post("/accounts/login/", {'username': 'paco', 'password': 'paco_pw'})
+        self.assertEquals(r.status_code, 302)  # redirect to index
+
+    def tearDown(self):
+        self.user.delete()
+
+class ViewLoggedInTests(LoggedInTests):
+    def setUp(self):
+        super(ViewLoggedInTests, self).setUp()
+        self.company = models.Company(name='Tienda 1')
+        self.company.save()
+        self.companyuser = models.CompanyUser(user=self.user, company=self.company)
+        self.companyuser.save()
+
+    def tearDown(self):
+        self.companyuser.delete()
+        self.company.delete()
+        super(ViewLoggedInTests, self).tearDown()
+
+    def test_view_index(self):
+        """
+        A logged-in user can view the billing index, and the index shows the available companies
+        """
+        response = self.c.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(list(response.context['companies']), [self.company])
+        self.assertContains(response, self.company.name)
--- tienda_ecuador_project/billing/views.py
+++ tienda_ecuador_project/billing/views.py
@@ -1,152 +1,167 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from models import Item, Bill, BillItem, ShopUser, Customer
-from forms import ItemForm, BillForm, BillItemForm
+from models import Item, Bill, BillItem, CompanyUser, Customer, Company
+# from forms import ItemForm, BillForm, BillItemForm
 from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def get_shop(request):
-    """
-    Tries to get the shop associated to an account
-    """
-    try:
-        user = request.user
-        shopuser = ShopUser.objects.get(user=user)
-        return shopuser.shop
-    except ShopUser.DoesNotExist:
-        raise Exception("Access Denied, you have access to no shops")
-
-
+# 
+# 
+# @login_required
+# def get_shop(request):
+    # """
+    # Tries to get the shop associated to an account
+    # """
+    # try:
+        # user = request.user
+        # shopuser = ShopUser.objects.get(user=user)
+        # return shopuser.shop
+    # except ShopUser.DoesNotExist:
+        # raise Exception("Access Denied, you have access to no shops")
+# 
+# 
 @login_required
 def index(request):
-    items = Item.objects.filter(shop=get_shop(request))
-    bills = Bill.objects.filter(shop=get_shop(request))
+    """
+    Shows an index for the current user
+    """
+    c_user = CompanyUser.objects.get(user=request.user)
     param_dict = {
-        'items': items,
-        'bills': bills,
+        'companies': Company.objects.filter(pk=c_user.company.pk),
     }
     return render(request, "billing/index.html", param_dict)
 
-
 @login_required
-def view_item(request, item_id):
-    param_dict = {
-        'item': get_object_or_404(Item, pk=item_id, shop=get_shop(request)),
-    }
-    return render(request, "billing/view_item.html", param_dict)
-
-
-@login_required
-def edit_item(request, item_id):
-    item = get_object_or_404(Item, pk=item_id, shop=get_shop(request))
-    if request.method == 'POST':
-        form = ItemForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save(commit=True)
-            return redirect("view_item", item_id)
-        else:
-            print form.errors
-    else:
-        form = ItemForm(instance=item)
-    return render(request, "billing/edit_item.html", {'form': form, 'item_id': item_id})
-
-
-@login_required
-def view_bill(request, bill_id):
-    bill = get_object_or_404(Bill, pk=bill_id)
-    param_dict = {
-        'bill': bill,
-    }
-    return render(request, "billing/view_bill.html", param_dict)
-
-
-@login_required
-def new_bill(request):
-    customer, created = Customer.objects.get_or_create(name='Consumidor Final')
-    new = Bill(shop=get_shop(request), issued_to=customer)
-    new.save()
-    return redirect("view_bill", new.pk)
-
-
-@login_required
-def edit_bill(request, bill_id):
-    bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
-    if not bill.can_be_modified():
-        # The bill has been issued, and can't be modified
-        raise Exception("The bill can't be modified")
-
-    if request.method == 'POST':
-        form = BillForm(request.POST, instance=bill)
-        if form.is_valid():
-            form.save(commit=True)
-            return redirect("view_bill", bill_id)
-        else:
-            print form.errors
-    else:
-        form = BillForm(instance=bill)
-    return render(request, "billing/edit_bill.html", {'form': form, 'bill_id': bill_id})
-
-
-@login_required
-def delete_bill(request, bill_id):
-    bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
-    if request.method == 'POST':
-        if bill.is_proforma:
-            items = BillItem.objects.find(bill=bill)
-            for item in items:
-                item.delete()
-            bill.delete()
-        else:
-            raise Exception("Bill is definitive")
-    return redirect("index")
-
-@login_required
-def add_item_to_bill(request, bill_id):
-    bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
-    if not bill.can_be_modified():
-        # The bill has been issued, and can't be modified
-        raise Exception("The bill can't be modified")
-
-    if request.method == 'POST':
-        item = Item.objects.get(pk=request.POST['item_id'], shop=get_shop(request))
-        values = dict(item.__dict__)
-        for k in values.keys():
-            if k.startswith('_') or k in ('id', 'baseitem_ptr_id'):
-                values.pop(k)
-        values['qty'] = 1
-        values['bill'] = bill
-        bitem = BillItem(**values)
-        bitem.save()
-        return redirect("view_bill", bill_id)
-    items = Item.objects.filter(shop=get_shop(request))
-    return render(request, "billing/add_item_to_bill.html", {'bill': bill, 'items': items})
-
-
-def edit_item_in_bill(request, bill_id, item_id):
-    bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
-    item = get_object_or_404(BillItem, pk=item_id, bill=bill, shop=get_shop(request))
-    if not bill.can_be_modified():
-        # The bill has been issued, and can't be modified
-        raise Exception("The bill can't be modified")
-
-    if request.method == 'POST':
-        form = BillItemForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save(commit=True)
-            return redirect("view_bill", bill_id)
-        else:
-            print form.errors
-    else:
-        form = BillItemForm(instance=item)
-    return render(request, "billing/edit_item_in_bill.html", {'form': form, 'bill_id': bill_id})
-
-def delete_item_in_bill(request, bill_id, item_id):
-    bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
-    item = get_object_or_404(BillItem, pk=item_id, bill=bill, shop=get_shop(request))
-    if not bill.can_be_modified():
-        # The bill has been issued, and can't be modified
-        raise Exception("The bill can't be modified")
-
-    if request.method == 'POST':
-        item.delete()
-    return redirect("view_bill", bill_id)
+def company_index(request, company_id):
+    """
+    Shows an index for a company
+    """
+    # items = Item.objects.filter(shop=get_shop(request))
+    # bills = Bill.objects.filter(shop=get_shop(request))
+    # param_dict = {
+        # 'items': items,
+        # 'bills': bills,
+    # }
+    param_dict = {}
+    return render(request, "billing/index.html", param_dict)
+# 
+# 
+# @login_required
+# def view_item(request, item_id):
+    # param_dict = {
+        # 'item': get_object_or_404(Item, pk=item_id, shop=get_shop(request)),
+    # }
+    # return render(request, "billing/view_item.html", param_dict)
+# 
+# 
+# @login_required
+# def edit_item(request, item_id):
+    # item = get_object_or_404(Item, pk=item_id, shop=get_shop(request))
+    # if request.method == 'POST':
+        # form = ItemForm(request.POST, instance=item)
+        # if form.is_valid():
+            # form.save(commit=True)
+            # return redirect("view_item", item_id)
+        # else:
+            # print form.errors
+    # else:
+        # form = ItemForm(instance=item)
+    # return render(request, "billing/edit_item.html", {'form': form, 'item_id': item_id})
+# 
+# 
+# @login_required
+# def view_bill(request, bill_id):
+    # bill = get_object_or_404(Bill, pk=bill_id)
+    # param_dict = {
+        # 'bill': bill,
+    # }
+    # return render(request, "billing/view_bill.html", param_dict)
+# 
+# 
+# @login_required
+# def new_bill(request):
+    # customer, created = Customer.objects.get_or_create(name='Consumidor Final')
+    # new = Bill(shop=get_shop(request), issued_to=customer)
+    # new.save()
+    # return redirect("view_bill", new.pk)
+# 
+# 
+# @login_required
+# def edit_bill(request, bill_id):
+    # bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
+    # if not bill.can_be_modified():
+        # # The bill has been issued, and can't be modified
+        # raise Exception("The bill can't be modified")
+# 
+    # if request.method == 'POST':
+        # form = BillForm(request.POST, instance=bill)
+        # if form.is_valid():
+            # form.save(commit=True)
+            # return redirect("view_bill", bill_id)
+        # else:
+            # print form.errors
+    # else:
+        # form = BillForm(instance=bill)
+    # return render(request, "billing/edit_bill.html", {'form': form, 'bill_id': bill_id})
+# 
+# 
+# @login_required
+# def delete_bill(request, bill_id):
+    # bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
+    # if request.method == 'POST':
+        # if bill.is_proforma:
+            # items = BillItem.objects.find(bill=bill)
+            # for item in items:
+                # item.delete()
+            # bill.delete()
+        # else:
+            # raise Exception("Bill is definitive")
+    # return redirect("index")
+# 
+# @login_required
+# def add_item_to_bill(request, bill_id):
+    # bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
+    # if not bill.can_be_modified():
+        # # The bill has been issued, and can't be modified
+        # raise Exception("The bill can't be modified")
+# 
+    # if request.method == 'POST':
+        # item = Item.objects.get(pk=request.POST['item_id'], shop=get_shop(request))
+        # values = dict(item.__dict__)
+        # for k in values.keys():
+            # if k.startswith('_') or k in ('id', 'baseitem_ptr_id'):
+                # values.pop(k)
+        # values['qty'] = 1
+        # values['bill'] = bill
+        # bitem = BillItem(**values)
+        # bitem.save()
+        # return redirect("view_bill", bill_id)
+    # items = Item.objects.filter(shop=get_shop(request))
+    # return render(request, "billing/add_item_to_bill.html", {'bill': bill, 'items': items})
+# 
+# 
+# def edit_item_in_bill(request, bill_id, item_id):
+    # bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
+    # item = get_object_or_404(BillItem, pk=item_id, bill=bill, shop=get_shop(request))
+    # if not bill.can_be_modified():
+        # # The bill has been issued, and can't be modified
+        # raise Exception("The bill can't be modified")
+# 
+    # if request.method == 'POST':
+        # form = BillItemForm(request.POST, instance=item)
+        # if form.is_valid():
+            # form.save(commit=True)
+            # return redirect("view_bill", bill_id)
+        # else:
+            # print form.errors
+    # else:
+        # form = BillItemForm(instance=item)
+    # return render(request, "billing/edit_item_in_bill.html", {'form': form, 'bill_id': bill_id})
+# 
+# def delete_item_in_bill(request, bill_id, item_id):
+    # bill = get_object_or_404(Bill, pk=bill_id, shop=get_shop(request))
+    # item = get_object_or_404(BillItem, pk=item_id, bill=bill, shop=get_shop(request))
+    # if not bill.can_be_modified():
+        # # The bill has been issued, and can't be modified
+        # raise Exception("The bill can't be modified")
+# 
+    # if request.method == 'POST':
+        # item.delete()
+    # return redirect("view_bill", bill_id)
