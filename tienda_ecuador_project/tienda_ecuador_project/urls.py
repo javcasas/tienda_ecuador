33061d24c56eb1fc4f7c807aca8238548a0b70f2
Javier Casas <javcasas@gmail.com>
2015-09-20 13:58:08 -0500

    Startup page v1 ready to be deployed

--- tienda_ecuador_project/tienda_ecuador_project/urls.py
+++ tienda_ecuador_project/tienda_ecuador_project/urls.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+import views
+
 
 urlpatterns = [
     # Examples:
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^form/$', TemplateView.as_view(template_name='request_form.html'), name='form'),  # FIXME:remove
-    url(r'^request_info/$', TemplateView.as_view(template_name='request_info.html'), name='request_info'),
-    url(r'^about/$', TemplateView.as_view(template_name='request_form.html'), name='about'),  # FIXME: use proper template
+    url(r'^request_info/$', views.RequestInfoView.as_view(template_name='request_info.html'), name='request_info'),
+    url(r'^about/$', views.RedirectToIndexView.as_view(), name='about'),
     url(r'^benefits/$', TemplateView.as_view(template_name='benefits.html'), name='benefits'),
-    url(r'^support/$', TemplateView.as_view(template_name='request_form.html'), name='support'),  # FIXME: use proper template
+    url(r'^support/$', views.SupportView.as_view(), name='support'),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^billing/', include('billing.urls')),
---
+++ tienda_ecuador_project/tienda_ecuador_project/views.py
@@ -0,0 +1,58 @@
+import pytz
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+from django.shortcuts import render, redirect
+from django.views.generic import View
+
+tz = pytz.timezone('America/Guayaquil')
+
+
+class RequestInfoView(View):
+    template_name = 'request_info.html'
+
+    def get(self, request):
+        return redirect("home")
+
+    def post(self, request):
+        msg = MIMEText('Interesado en DSSTI Facturas\nEmpresa: {}\nEmail: {}\n'.format(
+            request.POST['company'],
+            request.POST['email']))
+        msg["From"] = "ventas@dssti.com"
+        msg["To"] = "ventas@dssti.com"
+        msg["Subject"] = "Interesado en DSSTI Facturas"
+        p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+        p.communicate(msg.as_string())
+        return render(request, self.template_name, {})
+
+
+class RedirectToIndexView(View):
+    def get(self, request):
+        return redirect("home")
+
+
+class SupportView(View):
+    def get(self, request):
+        return render(request, 'support.html')
+
+    def post(self, request):
+        name = request.POST['name']
+        company = request.POST['company']
+        email = request.POST['email']
+        query_text = request.POST['query-text']
+        msg_text = u'''
+Peticion de Soporte
+Nombre: {name}
+Empresa: {company}
+Email: {email}
+
+{query_text}
+'''
+        msg = MIMEText(msg_text.format(name=name, company=company,
+                                       email=email, query_text=query_text))
+        msg["From"] = "soporte@dssti.com"
+        msg["To"] = "soporte@dssti.com"
+        msg["Subject"] = "Peticion de Soporte"
+        p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+        p.communicate(msg.as_string())
+        return render(request, 'support_submitted.html', {})
